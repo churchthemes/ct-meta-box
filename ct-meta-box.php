a9dfec0a79c5df7f072c9832b20657fe73d5a31e
Steven Gliebe <work@stevengliebe.com>
2017-05-24 13:29:36 -0500

    Change license string from GPL 2 URL to 'GPLv2 or later' so GPLv3 code can be used - same as WordPress core

--- ct-meta-box.php
+++ ct-meta-box.php
@@ -10,7 +10,7 @@
  * @package   CT_Meta_Box
  * @copyright Copyright (c) 2013 - 2016, churchthemes.com
  * @link      https://github.com/churchthemes/ct-meta-box
- * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * @license   GPLv2 or later
  */
 
 // No direct access
